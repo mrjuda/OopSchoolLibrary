0085a375146be99f7603799751a5a198cbe93a66
judah <87juda@gmail.com>
2022-09-03 23:34:41 -0300

    Add app.rb

---
+++ app.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+class App
+  puts 'Welcome to School Library App!'
+  puts ''
+  puts 'Please choose an option by entering a number'
+  puts '1 - List all Books'
+  puts '2 - List all People'
+  puts '3 - Create a Person'
+  puts '4 - Create a Book'
+  puts '5 - Create a Rental'
+  puts '6 - List all rentals for a given person id'
+  puts '7 - Exit'
+end
