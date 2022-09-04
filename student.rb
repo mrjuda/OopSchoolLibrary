60469c22b02d8e4912e34edac65e7fa0ae076257
judah <87juda@gmail.com>
2022-09-03 22:43:29 -0300

    Add rentals to student

--- student.rb
+++ student.rb
@@ -1,14 +1,18 @@
 # student.rb
 
+require_relative 'classroom'
 require './person'
 
 class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
     '¯\(ツ)/¯'
   end
+
+  attr_reader :classroom
 end
